7d3baa0ed2656c34b6152d446f7d87957f9245a6
denSmashh <s-noj02@yandex.ru>
2023-10-08 17:30:04 +0300

    change rtl, add apb state machine, task not sent all events in apb

--- QuickSilicon/6.Events_to_APB/events_to_apb.sv
+++ QuickSilicon/6.Events_to_APB/events_to_apb.sv
@@ -17,68 +17,103 @@ module events_to_apb
 
 typedef enum logic [1:0] {EVENT_A, EVENT_B, EVENT_C, NO_EVENT} event_t;
 
+typedef enum logic [1:0] {IDLE, SETUP, ACCESS} state_t;
+
+event_t req_event;
+//event_t last_event;
+state_t state, next_state;
+
 logic [31:0] event_a_cnt;
 logic [31:0] event_b_cnt;
 logic [31:0] event_c_cnt;
 
-logic write_req;
-logic write_transaction;
-event_t req_event;
 
 always_ff @(posedge clk or posedge reset) begin : EVENT_A_COUNTER
     if (reset) event_a_cnt <= 'b0;
+    else if(event_a_i && state == SETUP) event_a_cnt <= 'b1;
     else if (event_a_i) event_a_cnt <= event_a_cnt + 'b1;
     else ;
 end 
 
 always_ff @(posedge clk or posedge reset) begin : EVENT_B_COUNTER
     if (reset) event_b_cnt <= 'b0;
+    else if(event_b_i && state == SETUP) event_b_cnt <= 'b1;
     else if (event_b_i) event_b_cnt <= event_b_cnt + 'b1;
     else ;
 end 
 
 always_ff @(posedge clk or posedge reset) begin : EVENT_C_COUNTER
     if (reset) event_c_cnt <= 'b0;
+    else if(event_c_i && state == SETUP) event_c_cnt <= 'b1;
     else if (event_c_i) event_c_cnt <= event_c_cnt + 'b1;
     else ;
 end
 
-always_ff @(posedge clk or posedge reset) begin : DETECT_REQ
-    if (reset) write_req <= 'b0;
-    else if (apb_pready_i) write_req <= 'b0;
-    else if (event_a_i | event_b_i | event_c_i) write_req <= 'b1;
-    else ;
-end
 
-always_ff @(posedge clk or posedge reset) begin : WRITE_TRAN
-    if (reset) write_transaction <= 'b0;
-    else if (apb_pready_i) write_transaction <= 'b0;
-    else if (write_req) write_transaction <= 'b1;
-    else ;
-end
-
-always_ff @(posedge clk or posedge reset) begin : DETECT_EVENT
+always_ff @(posedge clk or posedge reset) begin : DETECT_REQ_EVENT
     if (reset) req_event <= NO_EVENT;
     else begin
-        if(event_a_i) req_event <= EVENT_A;
-        else if(event_b_i) req_event <= EVENT_B;
-        else if(event_c_i) req_event <= EVENT_C;
+        if(state == IDLE) begin
+            if (event_a_i) req_event <= EVENT_A;
+            else if (event_b_i) req_event <= EVENT_B;
+            else if (event_c_i) req_event <= EVENT_C;
+            else req_event <= NO_EVENT;
+        end
         else req_event <= NO_EVENT;
     end
 end
 
+// always_ff @(posedge clk or posedge reset) begin : SAVE_LAST_EVENT
+//     if (reset) last_event <= NO_EVENT;
+//     else if (state == SETUP) last_event <= req_event;
+//     else ;
+// end
+
+always_ff @(posedge clk or posedge reset) begin : TRANSITION_LOGIC
+    if (reset) state <= IDLE;
+    else state <= next_state;
+end
+
+always_comb begin
+    case (state)
+        IDLE: begin
+            apb_psel_o = 'b0;
+            apb_penable_o = 'b0;
+            apb_paddr_o = 'b0;
+            apb_pwdata_o = 'b0;
+            if (event_a_i | event_b_i | event_c_i) next_state = SETUP;
+            else next_state = IDLE;
+        end
+
+        SETUP: begin
+            apb_psel_o = 'b1;
+            apb_penable_o = 'b0;
+            apb_paddr_o  = (req_event == EVENT_A) ? 32'hABBA0000 :
+                           (req_event == EVENT_B) ? 32'hBAFF0000 :
+                           (req_event == EVENT_C) ? 32'hCAFE0000 : 32'b0;
+            apb_pwdata_o = (req_event == EVENT_A) ? event_a_cnt :
+                           (req_event == EVENT_B) ? event_b_cnt :
+                           (req_event == EVENT_C) ? event_c_cnt : 32'b0;
+            next_state = ACCESS;
+        end
+        
+        ACCESS: begin
+            apb_psel_o = 'b1;
+            apb_penable_o = 'b1;
+            if (apb_pready_i) next_state = IDLE;    // not supported back-to-back
+            else next_state = ACCESS;
+        end 
+        
+        default: begin 
+            apb_psel_o = 'b0;
+            apb_penable_o = 'b0;
+            apb_paddr_o = 'b0;
+            apb_pwdata_o = 'b0;
+            next_state = IDLE;
+        end
+    endcase
+end
+
 assign apb_pwrite_o = 1'b1;   // only write transaction
-
-assign apb_psel_o = (write_req) ? 'b1 : 'b0;
-
-assign apb_penable_o = (write_transaction) ? 'b1 : 'b0;
-
-assign apb_paddr_o = (write_transaction && req_event == EVENT_A) ? 32'hABBA0000 :
-                     (write_transaction && req_event == EVENT_B) ? 32'hBAFF0000 :
-                     (write_transaction && req_event == EVENT_C) ? 32'hCAFE0000 : 32'b0;
-
-assign apb_pwdata_o = (write_transaction && req_event == EVENT_A) ? event_a_cnt :
-                      (write_transaction && req_event == EVENT_B) ? event_b_cnt :
-                      (write_transaction && req_event == EVENT_C) ? event_c_cnt : 32'b0;
 
 endmodule
