c275b873bd4a49900ed2ccc8f5c6ba6bd2cb2858
denSmashh <s-noj02@yandex.ru>
2023-10-10 17:44:40 +0300

    change rtl, add working version, all verify in QuickSilicon tests

--- QuickSilicon/6.Events_to_APB/events_to_apb.sv
+++ QuickSilicon/6.Events_to_APB/events_to_apb.sv
@@ -20,54 +20,57 @@ typedef enum logic [1:0] {EVENT_A, EVENT_B, EVENT_C, NO_EVENT} event_t;
 typedef enum logic [1:0] {IDLE, SETUP, ACCESS} state_t;
 
 event_t req_event;
-//event_t last_event;
 state_t state, next_state;
 
-logic [31:0] event_a_cnt;
-logic [31:0] event_b_cnt;
-logic [31:0] event_c_cnt;
-
-
-always_ff @(posedge clk or posedge reset) begin : EVENT_A_COUNTER
-    if (reset) event_a_cnt <= 'b0;
-    else if(event_a_i && state == SETUP) event_a_cnt <= 'b1;
-    else if (event_a_i) event_a_cnt <= event_a_cnt + 'b1;
-    else ;
-end 
-
-always_ff @(posedge clk or posedge reset) begin : EVENT_B_COUNTER
-    if (reset) event_b_cnt <= 'b0;
-    else if(event_b_i && state == SETUP) event_b_cnt <= 'b1;
-    else if (event_b_i) event_b_cnt <= event_b_cnt + 'b1;
-    else ;
-end 
-
-always_ff @(posedge clk or posedge reset) begin : EVENT_C_COUNTER
-    if (reset) event_c_cnt <= 'b0;
-    else if(event_c_i && state == SETUP) event_c_cnt <= 'b1;
-    else if (event_c_i) event_c_cnt <= event_c_cnt + 'b1;
-    else ;
-end
+logic [3:0] event_a_cnt;
+logic [3:0] event_b_cnt;
+logic [3:0] event_c_cnt;
 
 
 always_ff @(posedge clk or posedge reset) begin : DETECT_REQ_EVENT
     if (reset) req_event <= NO_EVENT;
     else begin
         if(state == IDLE) begin
-            if (event_a_i) req_event <= EVENT_A;
-            else if (event_b_i) req_event <= EVENT_B;
-            else if (event_c_i) req_event <= EVENT_C;
+            if (event_a_i | (|event_a_cnt)) req_event <= EVENT_A;
+            else if (event_b_i | (|event_b_cnt)) req_event <= EVENT_B;
+            else if (event_c_i | (|event_c_cnt)) req_event <= EVENT_C;
             else req_event <= NO_EVENT;
         end
         else req_event <= NO_EVENT;
     end
 end
 
-// always_ff @(posedge clk or posedge reset) begin : SAVE_LAST_EVENT
-//     if (reset) last_event <= NO_EVENT;
-//     else if (state == SETUP) last_event <= req_event;
-//     else ;
-// end
+always_ff @(posedge clk or posedge reset) begin : EVENT_A_COUNTER
+    if (reset) event_a_cnt <= 'b0;
+    else if (state == SETUP && req_event == EVENT_A) begin
+        if (event_a_i) event_a_cnt <= 'b1;
+        else event_a_cnt <= 'b0; 
+    end
+    else if (event_a_i) event_a_cnt <= event_a_cnt + 'b1;
+    else ;
+end 
+
+always_ff @(posedge clk or posedge reset) begin : EVENT_B_COUNTER
+    if (reset) event_b_cnt <= 'b0;
+    else if (state == SETUP && req_event == EVENT_B) begin
+        if (event_b_i) event_b_cnt <= 'b1;
+        else event_b_cnt <= 'b0; 
+    end
+    else if (event_b_i) event_b_cnt <= event_b_cnt + 'b1;
+    else ;
+end
+
+always_ff @(posedge clk or posedge reset) begin : EVENT_C_COUNTER
+    if (reset) event_c_cnt <= 'b0;
+    else if (state == SETUP && req_event == EVENT_C) begin
+        if (event_c_i) event_c_cnt <= 'b1;
+        else event_c_cnt <= 'b0;
+    end
+    else if (event_c_i) event_c_cnt <= event_c_cnt + 'b1;
+    else ;
+end
+
+
 
 always_ff @(posedge clk or posedge reset) begin : TRANSITION_LOGIC
     if (reset) state <= IDLE;
@@ -81,7 +84,8 @@ always_comb begin
             apb_penable_o = 'b0;
             apb_paddr_o = 'b0;
             apb_pwdata_o = 'b0;
-            if (event_a_i | event_b_i | event_c_i) next_state = SETUP;
+            if (event_a_i | event_b_i | event_c_i | 
+               (|event_a_cnt) | (|event_b_cnt) | (|event_c_cnt)) next_state = SETUP;
             else next_state = IDLE;
         end
 
@@ -91,9 +95,9 @@ always_comb begin
             apb_paddr_o  = (req_event == EVENT_A) ? 32'hABBA0000 :
                            (req_event == EVENT_B) ? 32'hBAFF0000 :
                            (req_event == EVENT_C) ? 32'hCAFE0000 : 32'b0;
-            apb_pwdata_o = (req_event == EVENT_A) ? event_a_cnt :
-                           (req_event == EVENT_B) ? event_b_cnt :
-                           (req_event == EVENT_C) ? event_c_cnt : 32'b0;
+            apb_pwdata_o = (req_event == EVENT_A) ? {28'b0,event_a_cnt} :
+                           (req_event == EVENT_B) ? {28'b0,event_b_cnt} :
+                           (req_event == EVENT_C) ? {28'b0,event_c_cnt} : 32'b0;
             next_state = ACCESS;
         end
         
